f24ac306c004bb3d279dc5d457fc302f85df5ebd
Ion Jaureguialzo Sarasola <ion@jaureguialzo.com>
2026-03-26 12:08:07 +0100

    Página de demo con contenido

--- vue-demo/src/App.vue
+++ vue-demo/src/App.vue
@@ -1,11 +1,25 @@
-<script setup></script>
+<script setup>
+</script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+    <div class="d-flex vh-100 justify-content-center align-items-center">
+        <div class="card shadow" style="width: 30rem;">
+            <div class="card-body">
+                <h5 class="card-title">Demo</h5>
+                <h6 class="card-subtitle mb-2 text-secondary small text-opacity-50">Vue en GitHub Actions</h6>
+                <p class="card-text">Este es un proyecto de ejemplo sobre cómo desplegar
+                    una aplicación <a href="https://vuejs.org/" target="_blank">Vue</a>
+                    en <a href="https://docs.github.com/es/pages" target="_blank">GitHub Pages</a>
+                    usando <a href="https://docs.github.com/es/actions" target="_blank">GitHub Actions</a>.
+                </p>
+            </div>
+            <div class="card-footer p-3 text-end">
+                <a href="https://github.com/egibide-dam/demo-vue-github-actions"
+                   target="_blank"
+                   class="btn btn-primary">Ver el repositorio</a>
+            </div>
+        </div>
+    </div>
 </template>
 
 <style scoped></style>
